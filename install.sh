ffe4180fceb3c4eecd4500048ee38a5d18f1e7f4
Nashiito <84184848+Nashiito3@users.noreply.github.com>
2022-03-11 01:13:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ fi
 
 checkroot
 
-echo This tool has been coded by 0xStevenson,
+echo This tool has been coded by Nashiito,
 
-echo Copyright © 2022 0xStevenson - 0xbrut3f0rc3!#1109
+echo Copyright © 2022 Nashiito - Nashiito#2896
 echo Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions
 echo The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 echo THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
